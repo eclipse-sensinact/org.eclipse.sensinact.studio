e1d9d5be29647c26ec4e7e220b6fe48eae5bb89f
Etienne Gandrille <etienne.gandrille@cea.fr>
2018-12-03 16:19:08 +0100

    deconnection works again

--- services/org.eclipse.sensinact.studio.preferences/src/org/eclipse/sensinact/studio/preferences/StudioHttpConfig.java
+++ services/org.eclipse.sensinact.studio.preferences/src/org/eclipse/sensinact/studio/preferences/StudioHttpConfig.java
@@ -28,11 +28,11 @@ public class StudioHttpConfig {
 
 	private final int port;
 	private final URL url;
-	
+
 	private static final Logger logger = Logger.getLogger(StudioHttpConfig.class);
-	
+
 	public StudioHttpConfig(GatewayHttpConfig gwConfig, int port) {
-		this.port = port;		
+		this.port = port;
 		URL url = null;
 		try {
 			url = computeStudioURL(port, gwConfig);
@@ -45,17 +45,17 @@ public class StudioHttpConfig {
 	public int getPort() {
 		return port;
 	}
-	
+
 	public URL getUrl() throws IOException {
 		if (isValid())
 			return url;
 		throw new IOException("Didn't mange to compute the Studio URL");
 	}
-	
+
 	public boolean isValid() {
 		return url != null;
 	}
-	
+
 	private static URL computeStudioURL(int studioPort, GatewayHttpConfig gwConfig) throws IOException {
 		try {
 			String gwHost = gwConfig.getURL().getHost();
@@ -66,7 +66,7 @@ public class StudioHttpConfig {
 			throw new IOException("Can't compute studio URL", e);
 		}
 	}
-	
+
 	private static String getHostAddress(InetAddress gatewayAddress, int timeout) throws SocketException, IOException {
 		Enumeration<NetworkInterface> localInterfaces = NetworkInterface.getNetworkInterfaces();
 		logger.info("Gateway address: " + gatewayAddress.getHostAddress());
@@ -74,57 +74,60 @@ public class StudioHttpConfig {
 			NetworkInterface localInterface = localInterfaces.nextElement();
 			logger.info(" * Analyzing Interface: " + localInterface.getInterfaceAddresses());
 
-			Enumeration<InetAddress> addresses = localInterface.getInetAddresses();
 			
-			if(localInterface.isUp())
-			while (addresses.hasMoreElements()) {
-				InetAddress address = addresses.nextElement();
-				logger.info("    - Verify IPv4: " + address.getHostAddress());
-				if (address instanceof Inet4Address) {
-					logger.debug("    - Checking IPv4 address: " + address);
-					String retval = address.getHostAddress();
-					if (retval != null && ! retval.isEmpty() && gatewayAddress.isReachable(localInterface, 64, timeout)) {
-						logger.info("--> Selected for Studio IP --->"+retval);
-						return retval;
+			if (localInterface.isUp()) {
+				Enumeration<InetAddress> addresses = localInterface.getInetAddresses();
+				while (addresses.hasMoreElements()) {
+					InetAddress address = addresses.nextElement();
+					logger.info("    - Verify IPv4: " + address.getHostAddress());
+					if (address instanceof Inet4Address) {
+						logger.debug("    - Checking IPv4 address: " + address);
+						String retval = address.getHostAddress();
+						if (retval != null && !retval.isEmpty()) {					
+							if (! retval.equals("127.0.0.1")) {
+								logger.info("--> Selected for Studio IP --->" + retval);
+								return retval;
+							}
+						}
+					} else if (address instanceof Inet6Address) {
+						logger.debug("    - IPv6 address ignored: " + address);
 					}
-				} else if (address instanceof Inet6Address) {
-					logger.debug("    - IPv6 address ignored: " + address);
 				}
-			}			
+			}
 		}
 		throw new IOException("Can't compute studio IP address");
 	}
-	
+
 	public boolean equals(Object other) {
 		if (this == other)
 			return true;
 		if (!(other instanceof StudioHttpConfig))
 			return false;
 		final StudioHttpConfig that = (StudioHttpConfig) other;
-		
+
 		if (this.getPort() != that.getPort())
 			return false;
-		
+
 		URL thisURL = null;
 		try {
 			thisURL = this.getUrl();
 		} catch (IOException e) {
 			// doNothing
 		}
-		
+
 		URL thatURL = null;
 		try {
 			thatURL = that.getUrl();
 		} catch (IOException e) {
 			// doNothing
 		}
-		
+
 		if (thisURL == null)
 			return thatURL == null;
 		return thisURL.equals(thatURL);
 	}
 
 	public int hashCode() {
-		return url.hashCode() % (port+7);
+		return url.hashCode() % (port + 7);
 	}
 }
